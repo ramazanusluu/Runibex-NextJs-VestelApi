dd7cde9547fd22701d45468dcdd0b6f8a96ea34c
Ramazan USLU <ruslu1720@gmail.com>
2022-08-18 22:14:26 +0300

    getServerSideProps ile subCategory verileri console yazdirildi

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,9 +1,21 @@
 import React from "react";
 
-export default function SubCategory() {
+export default function SubCategory({ subCategory }) {
+  console.log(subCategory);
   return (
     <div>
       <h1>SubCategor Page Component</h1>
     </div>
   );
 }
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://store.vrunibex.com/mobile2/mbProduct/ProductList?CategoryID=${context.params.id}`
+  );
+  const subCategory = await res.json();
+  return {
+    props: {
+      subCategory,
+    },
+  };
+};
