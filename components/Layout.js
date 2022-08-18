3652652a9fb6d20f0632226872c6f6bbe469bb4c
Ramazan USLU <ruslu1720@gmail.com>
2022-08-18 15:29:32 +0300

    Navbar componenti olusturuldu & _document.js dosyası eklenerek bootstrap icin gerekli olan bundle ve css dosyalarida eklendi

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Navbar from "./Navbar";
 
 function Layout({ children }) {
   return (
     <>
       <Header />
+      <Navbar />
       <div className="container">
         <main>{children}</main>
       </div>
---
+++ components/Navbar.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-vestel">
+        <div className="container">
+          {!isOpen && (
+            <button
+              className="btn btn-danger btn-navbar fs-6 active"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <i className="fa-solid fa-bars me-2"></i>
+              Tüm Ürünler
+            </button>
+          )}
+          {isOpen && (
+            <button
+              className="btn btn-danger btn-navbar fs-6  active"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <i className="fa-solid fa-arrow-left me-2"></i>
+              Tüm Ürünler
+            </button>
+          )}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item me-2">
+                {/* <a className="nav-link active" href="/">
+                  <i className="fa-solid fa-house me-2"></i>
+                  Ana Sayfa
+                </a> */}
+                <Link href="/">
+                  <button className="btn btn-danger btn-navbar fs-6 active">
+                    <i className="fa-solid fa-house me-2"></i>
+                    Ana Sayfa
+                  </button>
+                </Link>
+              </li>
+              <li className="nav-item me-2">
+                {/* <a className="nav-link active" href="/">
+                  <i className="fa-solid fa-magnifying-glass me-2"></i>
+                  Ara
+                </a> */}
+                <Link href="/">
+                  <button className="btn btn-danger btn-navbar fs-6 active">
+                    <i className="fa-solid fa-magnifying-glass me-2"></i>
+                    Ara
+                  </button>
+                </Link>
+              </li>
+              <li className="nav-item">
+                {/* <a className="nav-link active" href="/">
+                  <i className="fa-solid fa-filter me-2"></i>
+                  Filtrele
+                </a> */}
+                <Link href="/">
+                  <button className="btn btn-danger btn-navbar fs-6 active">
+                    <i className="fa-solid fa-filter me-2"></i>
+                    Filtrele
+                  </button>
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="/">
+                  <i className="fa-solid fa-basket-shopping"></i>
+                  <span className="translate-middle badge rounded-pill bg-light text-dark rounded-circle">
+                    0
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {isOpen && <Sidebar />}
+    </div>
+  );
+}
+
+export default Navbar;
