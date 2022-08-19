ff7c10041b177f1310392a2c60444dd2b4162408
Ramazan USLU <ruslu1720@gmail.com>
2022-08-19 17:39:35 +0300

    Sidebar menu icin category listesinin cekilmesi

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,26 @@
 import React from "react";
 import style from "../styles/Sidebar.module.css";
+import { useState, useEffect } from "react";
 
 function Sidebar() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://store.vrunibex.com/mobile2/mbProduct/CategoryList")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
+  console.log(data);
+
   return (
     <div className="container">
       <div className={style.sidebar}>
